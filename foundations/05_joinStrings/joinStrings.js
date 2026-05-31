6783497174caf6858651794fe36c4694730b7a04
Anca <ancalazar333@gmail.com>
2026-05-31 21:34:33 +0300

    done with the first lesson

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -9,9 +9,15 @@
 
 // Add your code right below, good luck!
 
+firstName = "Carlos";
+lastName = "Stevenson";
+thisYear = 1965;
+birthYear = 1947;
 
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
-
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
 // Do not change this
 module.exports = {
